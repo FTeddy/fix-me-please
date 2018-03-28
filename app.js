7295b2a03c80bae7bb4c1d62774dbd2addefdeba
FTeddy <ainsophoure@gmail.com>
2018-03-28 09:58:56 +0700

    book post fixed

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
+let url = 'mongodb://werkbau:YratyortSevaC5691@devstack-0-shard-00-00-hjaoe.mongodb.net:27017,devstack-0-shard-00-01-hjaoe.mongodb.net:27017,devstack-0-shard-00-02-hjaoe.mongodb.net:27017/fixMe?ssl=true&replicaSet=devstack-0-shard-0&authSource=admin'
+let localhost = 'mongodb://localhost/api-crud-mongoose'
+mongoose.connect(url, (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
 var books = require('./routes/books');
 var transactions = require('./routes/transactions');
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:false }))
 app.use('/books', books);
 app.use('/transactions', transactions);
 
--- controllers/books.js
+++ controllers/books.js
@@ -1,37 +1,53 @@
+const Book = require('../models/Book.js')
+
 module.exports = {
   all: function(req, res) {
     Book.find(function (err, books) {
       if (err) {
-        res.send({err: err})
+        res.status(500).send({err: err})
       }
-      res.send(books)
+      res.status(200).send(books)
     })
   },
   create: function(req, res) {
-    var book = new Book(req.body);
+    console.log(req.body);
+    var book = new Book({
+      isbn: req.body.isbn,
+      title: req.body.title,
+      author: req.body.author,
+      category: req.body.category,
+      stock: Number(req.body.stock),
+    });
     book.save(function (err, result) {
       if (err) {
-        res.send({err: err})
+        res.status(500).send({err: err})
       }
-      res.send(result)
+      res.status(200).send(result)
     });
   },
   update: function(req, res) {
+    let updataData = {}
+    if (req.body.isbn) {updateData.isbn = req.body.isbn}
+    if (req.body.title) {updateData.title = req.body.title}
+    if (req.body.author) {updateData.author = req.body.author}
+    if (req.body.category) {updateData.category = req.body.category}
+    if (req.body.stock) {updateData.stock = Number(req.body.stock)}
+
     Book.update({ _id: req.id }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
-        res.send({err: err})
+        res.status(500).send({err: err})
       }
-      res.send(result)
+      res.status(200).send(result)
     });
   },
   delete: function(req, res) {
     Book.remove({ _id: req.id }, function (err, result) {
       if (err) {
-        res.send({err: err})
+        res.status(500).send({err: err})
       }
-      res.send(result)
+      res.status(200).send(result)
     });
   }
 }
